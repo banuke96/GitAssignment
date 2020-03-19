96bb8a4da7200926f5786f511fd19f0cf062af6b
Toshiba <Toshiba@DESKTOP-D5H07MU.local>
2020-03-19 16:59:09 +0200

    Primul commit

---
+++ temaCTS/src/temaCTS/TestGit.java
@@ -0,0 +1,11 @@
+package temaCTS;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Git ! Denumirea proiectului de licenta este Mobile Learning in IT");
+		
+	}
+
+}
