075ccfc4a8055ac0052befc911e6f30ed0bd7799
Toshiba <Toshiba@DESKTOP-D5H07MU.local>
2020-03-19 17:10:22 +0200

    Am creat clasa Lectie, ce contine trei atribute si un constructor.

---
+++ temaCTS/src/temaCTS/Lectie.java
@@ -0,0 +1,16 @@
+package temaCTS;
+
+public class Lectie {
+	private int id;
+	private String titlu;
+	private String textulLectiei;
+	
+	
+	public Lectie(int id, String titlu, String textulLectiei) {
+		this.id = id;
+		this.titlu = titlu;
+		this.textulLectiei = textulLectiei;
+	}
+	
+	
+}
